8e20af3ea5b15adcff272d668de368dca0bc6005
Adriano Sousa <adrianodesousa1998@gmail.com>
2024-05-27 22:15:33 -0300

    Modificação

--- Program.cs
+++ Program.cs
@@ -6,7 +6,111 @@ namespace DescontoDeSalário
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao = 0;
+            Console.WriteLine("Seja bem vindo ao nosso sistema de processamento de olerite.");
+
+            while (opcao == 0)
+            {
+                Console.WriteLine("Escolha uma opção:");
+                Console.WriteLine("1: Iniciar;");
+                Console.WriteLine("2: Sair;");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        ProcessaHolerite();
+                        opcao = 0;
+                        break;
+                    case 2:
+                        Console.WriteLine("Obrigado por usar nosso sistema!");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        public static void ProcessaHolerite()
+        {
+            Console.WriteLine("Digite o seu valor hora.");
+            double valorHora = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite quantas horas trabalhadas.");
+            int horaTrabalhada = int.Parse(Console.ReadLine());
+
+            double salarioBruto = CalculaSalarioBruto(horaTrabalhada, valorHora);
+            double contribuicao = CalculaContribuicao(salarioBruto);
+            double INSS = CalculaINSS(salarioBruto);
+            double IR = CalculaIR(salarioBruto);
+            double FGTS = CalculaFGTS(salarioBruto);
+            double totalDescontos = IR + INSS + contribuicao;
+            double salarioLiquido = salarioBruto - totalDescontos;
+            
+        }
+
+        public static void ImprimeOlerite(double salarioBruto, double IR, double INSS, double FGTS, double totalDescontos, double salarioLiquido)
+        {
+            Console.WriteLine($"Salário bruto: R$ {salarioBruto.ToString("C")}\n");
+            Console.WriteLine($"IR: R$ {IR.ToString("C")}\n");
+            Console.WriteLine($"INSS: R$ {INSS.ToString("C")}\n");
+            Console.WriteLine($"FGTS: R$ {FGTS.ToString("C")}\n");
+            Console.WriteLine($"Total de descontos: R$ {totalDescontos.ToString("C")}\n");
+            Console.WriteLine($"Salário líquido: R$ {salarioLiquido.ToString("C")}\n");
+        }
+        
+        public static double CalculaSalarioBruto(int horas, double valorHora)
+        {
+            return valorHora * horas;
+        }
+
+        public static double CalculaContribuicao(double salarioBruto)
+        {
+            return salarioBruto * 0.03;
+        }
+
+        public static double CalculaFGTS(double salarioBruto)
+        {
+            return salarioBruto * 0.11;
+        }
+
+        public static double CalculaINSS(double salarioBruto)
+        {
+            return salarioBruto * 0.10;
+        }
+
+        public static double CalculaIR(double salarioBruto)
+        {
+            if (salarioBruto < 0)
+            {
+                Console.WriteLine("O salário não pode ser menor que 0.");
+                return 0;
+            }
+
+            if (salarioBruto <= 900)
+            {
+                return 0;
+            }
+
+            else if (salarioBruto > 900 && salarioBruto <= 1500)
+            {
+                return salarioBruto * 0.05;
+            }
+
+            else if (salarioBruto > 1500 && salarioBruto <= 3500)
+            {
+                return salarioBruto * 0.1;
+            }
+
+            else if (salarioBruto > 3500)
+            {
+                return salarioBruto * 0.2;
+            }
+
+            return 0;
         }
     }
 }
+
+    
+
--- obj/Debug/netcoreapp3.1/DescontoDeSalário.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/DescontoDeSalário.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DescontoDeSalário")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5046c998c12fd29489e449f9c55c7fc090a2f7e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+145db27a3b99cf4a80105df552d401c20e712757")]
 [assembly: System.Reflection.AssemblyProductAttribute("DescontoDeSalário")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DescontoDeSalário")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
